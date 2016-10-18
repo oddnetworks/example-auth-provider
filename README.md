3053942e3f598e8da73f0327942fa69afe16ee49
Blain Smith <rebelgeek@blainsmith.com>
2016-10-18 11:43:58 -0400

    init

--- README.md
+++ README.md
@@ -1,2 +1,78 @@
-# example-auth-provider
-Example implementation of a 3rd party auth provider for Oddworks.
+# Example Auth Provider
+
+Example implementation of a 3rd party auth provider for Oddworks. This should guide developers build routes on their webside so that Oddworks powered app can authenticate viewers to login and watch content.
+
+Your website should implement the routes described below. While the actual routes do not matter, since they are configurable in Oddworks, the responses and payloads do matter so that Oddworks can interpret them correctly.
+
+## Running Example Server
+
+```bash
+$ npm run dev
+```
+
+This will start the example server on http://localhost:3000 with `/login` and `/verify` routes.
+
+## `POST http://yourdomain.com/login`
+
+**Oddworks Request**
+
+```json
+{
+  "type": "authentication",
+  "attributes": {
+    "email": "viewer@yourdomain.com",
+    "password": "pass12345"
+  }
+}
+```
+
+**Your Server Response**
+
+When Oddworks posts to your login route it should respond with a **HTTP 200** with the following payload so that Oddworks can stay in sync.
+
+- `id` - this is the record number of your viewer/user on your end
+- `email` - Oddworks will keep this as the canonical ID in our system so that we can look up users by email quickly when serving content
+- `jwt` - this is YOUR JWT used later on when Oddworks needs to re-verify users so we can login on their behalf without requiring Oddworks to store their password locally.
+
+```json
+{
+  "id": "YOUR_VIEWER_RECORD_ID",
+  "type": "viewer",
+  "attributes": {
+    "email": "viewer@yourdomain.com"
+  },
+  "meta": {
+    "jwt": "YOUR_SIGNED_JWT"
+  }
+}
+```
+
+## `POST http://yourdomain.com/verify`
+
+**Oddworks Request**
+
+```json
+{
+  "type": "authorization",
+  "attributes": {
+    "jwt": "THE_JWT_YOU_SENT_FROM_ABOVE"
+  }
+}
+```
+
+**Your Server Response**
+
+Should return the same response as above since Oddworks is logging in as a user with a JWT instead. The only exception is that the response should contain an updated JWT for the user.
+
+```json
+{
+  "id": "YOUR_VIEWER_RECORD_ID",
+  "type": "viewer",
+  "attributes": {
+    "email": "viewer@yourdomain.com"
+  },
+  "meta": {
+    "jwt": "YOUR_SIGNED_JWT"
+  }
+}
+```
---
+++ index.js
@@ -0,0 +1,110 @@
+const _ = require('lodash');
+const express = require('express');
+const bodyParser = require('body-parser');
+const jwt = require('jsonwebtoken');
+
+const app = express();
+
+// Mock DB of user records
+const DB = [
+	{id: 1, email: 'user1@example.com', password: 'pass12345'},
+	{id: 2, email: 'user2@example.com', password: 'pass12345'},
+	{id: 3, email: 'user3@example.com', password: 'pass12345'}
+];
+
+app.use(bodyParser.json());
+
+app.post('/login', (req, res) => {
+	const payload = req.body.data;
+
+	try {
+		// Verify the payload type is authentication, if not then fail
+		if (payload.type === 'authentication') {
+
+			// Look up the user in the DB
+			const user = _.find(DB, {email: payload.attributes.email});
+
+			// If a user is found AND the passwords match then respond with a HTTP 200 with a viewer
+			if (user && user.password === payload.attributes.password) {
+				res.status(200).send({
+					data: {
+						id: user.id,
+						type: 'viewer',
+						attributes: {
+							email: user.email
+						},
+						meta: {
+							jwt: jwt.sign({
+								iss: 'example.com',
+								sub: user.id
+							},
+							'secret',
+							{
+								expiresIn: '5 days'
+							})
+						}
+					}
+				});
+			} else {
+				res.status(403).send('login failed');
+			}
+		} else {
+			res.status(400).send('type is not authentication')
+		}
+	} catch (err) {
+		res.status(500).send(err.message);
+	}
+});
+
+app.post('/verify', (req, res) => {
+	const payload = req.body.data;
+
+	try {
+		// Verify the payload type is authorization, if not then fail
+		if (payload.type === 'authorization') {
+
+			// If the payload does not have a JWT then fail it
+			if (payload.attributes.jwt && payload.attributes.jwt !== '') {
+				try {
+					// Verify the JWT sent, if not verified then fail it
+					const decoded = jwt.verify(payload.attributes.jwt, 'secret');
+
+					// If valid JWT look up the user in the DB based on the jwt.sub and respond with a HTTP 200 with a viewer
+					const user = _.find(DB, {id: decoded.sub});
+
+					res.status(200).send({
+						data: {
+							id: user.id,
+							type: 'viewer',
+							attributes: {
+								email: user.email
+							},
+							meta: {
+								jwt: jwt.sign({
+									iss: 'example.com',
+									sub: user.id
+								},
+								'secret',
+								{
+									expiresIn: '5 days'
+								})
+							}
+						}
+					});
+				} catch (err) {
+					res.status(403).send('verification failed');
+				}
+			} else {
+				res.status(403).send('verification failed');
+			}
+		} else {
+			res.status(400).send('type is not authorization')
+		}
+	} catch (err) {
+		res.status(500).send(err.message);
+	}
+});
+
+app.listen(process.env.PORT, () => {
+	console.log(`Listening on port ${process.env.PORT}`);
+})
